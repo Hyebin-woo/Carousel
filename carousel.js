bfa2d2b110426d702fc5d955f4e707934412d880
Hyebin-Woo <dev.hyebin@gmail.com>
2022-05-16 00:26:06 +0900

    과제

--- carousel.js
+++ carousel.js
@@ -22,16 +22,21 @@ function translateContainer(direction){
   const selectedBtn = (direction === 1) ? 'prev' : 'next';
   carousel.style.transitionDuration = '500ms';
   carousel.style.transform = `rotateY(${direction * 60}deg)`;
+  // 동작이 종료시 
   carousel.ontransitionend = () => reorganizeEl(selectedBtn);
 }
 
+// 요소들을 재구성하는 함수
 function reorganizeEl(selectedBtn) {
     carousel.removeAttribute('style');
 
     if(selectedBtn == 'prev') {
-      carousel.insertBefore(carousel.lastElementChild, carousel.firstElementChild)
+      // 참조된 노드 앞에 특정 부모 노드의 자식 노드를 삽입
+      // prev 누르면 마지막 요소가 첫번째 요소가 된다. 
+      carousel.insertBefore(carousel.lastElementChild, carousel.firstElementChild);
     }
     else {
+      // 첫번째 요소가 자식요소로 된다. 
       carousel.appendChild(carousel.firstElementChild);
     }
   }
